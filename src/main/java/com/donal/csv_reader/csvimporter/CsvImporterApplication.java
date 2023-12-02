f05dc7a650af3489b1a773a846d9bb9889c4af1b
erhnaks <erhnaks@hotmail.com>
2023-12-02 10:43:42 +0000

    Initial commit CSV Importer Project for Donal R.

---
+++ src/main/java/com/donal/csv_reader/csvimporter/CsvImporterApplication.java
@@ -0,0 +1,13 @@
+package com.donal.csv_reader.csvimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donal/csv_reader/csvimporter/CsvImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donal.csv_reader.csvimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
