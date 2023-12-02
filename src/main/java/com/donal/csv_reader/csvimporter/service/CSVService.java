0e897a0ad72fa83b4108e2a1f5c161d7a9992612
erhnaks <erhnaks@hotmail.com>
2023-12-02 13:23:09 +0000

    CSV Importer/reader Service layer has been implemented

---
+++ src/main/java/com/donal/csv_reader/csvimporter/service/CSVService.java
@@ -0,0 +1,38 @@
+package com.donal.csv_reader.csvimporter.service;
+
+import com.donal.csv_reader.csvimporter.importer.CSVImporter;
+import com.donal.csv_reader.csvimporter.model.CustomerDetail;
+import com.donal.csv_reader.csvimporter.repository.CustomerDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class CSVService {
+
+    private CustomerDetailsRepository repository;
+
+    @Autowired
+    public CSVService(CustomerDetailsRepository repository) {
+    }
+
+    public void save(MultipartFile file) {
+        try {
+            List<CustomerDetail> customerDetails = CSVImporter.customerDetails(file.getInputStream());
+            repository.saveAll(customerDetails);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to store data: " + e.getMessage());
+        }
+    }
+
+    public List<CustomerDetail> getAllCustomers() {
+        return repository.findAll();
+    }
+
+    public List<CustomerDetail> getCustomerByReference(String reference) {
+        return repository.findByCustomerReference(reference);
+    }
+}
