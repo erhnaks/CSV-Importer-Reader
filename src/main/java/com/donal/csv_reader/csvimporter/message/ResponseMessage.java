56529da83f87eb50695b6c802adb2050435bc606
erhnaks <erhnaks@hotmail.com>
2023-12-03 11:10:07 +0000

    Enhanced Repsonse messages

--- src/main/java/com/donal/csv_reader/csvimporter/message/ResponseMessage.java
+++ src/main/java/com/donal/csv_reader/csvimporter/message/ResponseMessage.java
@@ -3,6 +3,10 @@ package com.donal.csv_reader.csvimporter.message;
 public class ResponseMessage {
     private String message;
 
+    public static String IMPORT_SUCCESS_MESSAGE = "The CSV file '%s' has been successfully uploaded to the server.";
+    public static String CSV_FORMAT_NOT_SUPPORTED = "Sorry, only CSV files are supported!";
+    public static String BAD_REQUEST_MESSAGE = "Sorry, the request could not be processed due to invalid or missing parameters. Please check your input and try again.";
+
     public ResponseMessage(String message) {
         this.message = message;
     }
